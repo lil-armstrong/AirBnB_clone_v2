e6ab19b430aa4b0a64cb19633abc1e6a25492cea
lil-armstrong <oebong1@gmail.com>
2023-05-25 20:30:38 +0100

    refactor

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' script that starts a Flask web application'''
+"""script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,5 +7,9 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def hello_world():
-    '''GET route to /'''
+    """GET route to /"""
     return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
