f1a35cdf1c2c84cdbfa986c114787455271ab715
lil-armstrong <oebong1@gmail.com>
2023-05-25 20:18:21 +0100

    refactor

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application'''
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,4 +7,5 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def hello_world():
+    '''GET route to /'''
     return "Hello HBNB!"
